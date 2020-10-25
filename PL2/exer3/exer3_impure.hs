be2d45eea1c0d753fe0ae322fe631a19b4d86102
jasonNikolaou <nikolaouiasonas13@gmail.com>
2020-10-25 16:25:00 +0200

    Change exer3_impure.hs

--- PL2/exer3/exer3_impure.hs
+++ PL2/exer3/exer3_impure.hs
@@ -1,8 +1,11 @@
+import Data.Array ((!), listArray, elems)
+import Data.List (foldl', scanl')
 import Control.Monad (replicateM)
 import qualified Data.ByteString.Char8 as C
 import Data.ByteString.Lazy.Builder
 import Data.ByteString.Lazy.Builder.ASCII
 import Data.Int
+import Data.Monoid
 import System.IO
 
 sums = [1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383,32767,65535,131071,262143,524287]
@@ -17,27 +20,28 @@ dp n m = elems list
 
 constructSums list m = tail $ scanl' (\acc x -> (acc+x) `mod` m) 0 list
 
-solve sumsArr m (a,b) = let dp_a = if a == 0 then 1 else sumsArr ! (a-1)
+solve sumsArr m [a,b] = let dp_a = if a == 0 then 1 else sumsArr ! (a-1)
                             dp_b = sumsArr ! b
                         in (dp_b - dp_a) `mod` m
 
-toTuple = (\(x:y:[]) -> (x,y))
+findMax = foldl' (\acc s -> let [_, b] = s in (max acc b)) 0
 
 readInts :: IO [Int64]
 readInts = map parse . C.words <$> C.getLine
  where parse s = let Just (n, _) = C.readInteger s
                  in fromIntegral n
 
-findMax = foldl' (\acc s -> let (_, b) = s in (max acc b)) 0
+fastPrint answers = hPutBuilder stdout $ build  answers
+build = foldr add_line mempty
+  where add_line n b = int64Dec n <> charUtf8 '\n' <> b
 
 main = do
   (n:m:[]) <- readInts
-  queries_ <- replicateM (fromIntegral n) readInts
-  let queries = map toTuple queries_
-  let max' = (findMax queries) + 2
+  queries <- replicateM (fromIntegral n) readInts
+  let max' = findMax(queries) + 1
   let dpList = dp max' m
   let partialSums = constructSums dpList m
   let partialSumsArray = listArray (0, max') partialSums
   let answers = map (solve partialSumsArray m) queries
 
-  mapM_ print answers 
+  fastPrint answers
