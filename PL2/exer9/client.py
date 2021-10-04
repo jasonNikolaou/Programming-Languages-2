0d11e6557cc78dbee42dea6026071aebb83d477d
jasonNikolaou <35314211+jasonNikolaou@users.noreply.github.com>
2021-10-04 18:51:03 +0300

    Add exercise 9

---
+++ PL2/exer9/client.py
@@ -0,0 +1,35 @@
+import requests, math
+from bs4 import BeautifulSoup
+
+def calcSol(n, k, p):
+    return math.comb(n, k) % p
+
+URL = 'http://127.0.0.1:5000'
+session = requests.Session()
+page = session.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+rounds = int(soup.find(id='totalQuestions').text)
+
+#play the game
+for _ in range(rounds):
+    questionNum = int(soup.find(id='questionNum').text)
+    n = int(soup.find(id='n').text)
+    k = int(soup.find(id='k').text)
+    p = int(soup.find(id='p').text)
+    print(f'Question {questionNum}:')
+    print(f'Calculate C({n},{k}) modulo {p}:')
+    print(f'{calcSol(n, k, p)}')
+    res = session.post(URL, data={'answer': calcSol(n, k, p)})
+    soup = BeautifulSoup(res.content, 'html.parser')
+    answer = soup.find(id='answer').text
+    if answer == "Correct":
+        print('Correct answer!')
+    else:
+        print('Wrong answer...')
+    score = soup.find(id='score')
+    if score:
+        print('End of game')
+        print(f'Score: {score.text}')
+    else:
+        page = session.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
